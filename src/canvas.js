94865699ab21138e62615ccf2fcaf47f9b110994
LukeMacdonald <lukemacdonald21@gmail.com>
2023-09-14 19:19:00 +1000

    first attempt at merge projects

---
+++ src/canvas.js
@@ -0,0 +1,138 @@
+import React, { Component } from "react";
+import { createRoot } from "react-dom/client";
+import InfiniteScroll from "react-infinite-scroller";
+import markdownit from "markdown-it";
+import { FormattedMessage } from "react-intl";
+import { WrappedIntlProvider } from "./react-components/wrapped-intl-provider";
+import { AuthContextProvider } from "./react-components/auth/AuthContext";
+import { store } from "./utils/store-instance";
+import { Row, Col } from "react-bootstrap";
+import Course from "./react-components/canvas/Course";
+
+window.APP = { store };
+
+import registerTelemetry from "./telemetry";
+import "./react-components/styles/global.scss";
+import "./assets/stylesheets/whats-new.scss";
+import { PageContainer } from "./react-components/layout/PageContainer";
+import { Spinner } from "./react-components/misc/Spinner";
+import { ThemeProvider } from "./react-components/styles/theme";
+
+registerTelemetry("/whats-new", "Hubs What's New");
+
+function formatDate(value) {
+  return value && new Date(value).toLocaleDateString(undefined, { month: "short", day: "numeric", year: "numeric" });
+}
+
+const md = markdownit();
+const DOMAIN = "http://131.170.250.239:49152";
+
+async function getCourses() {
+  try {
+    const endpoint = `${DOMAIN}/course/teacher`;
+
+    const params = { method: "GET", headers: { "Content-Type": "application/json" } };
+
+    const response = await fetch(endpoint, params);
+    const courses = await response.json();
+    return courses;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+}
+
+class Canvas extends Component {
+  state = {
+    pullRequests: [],
+    moreCursor: null,
+    hasMore: true,
+    currentDate: null
+  };
+
+  async getCanvas() {
+    const endpoint = "/api/v1/canvas";
+    const params = ["source=hubs", this.state.moreCursor ? `cursor=${this.state.moreCursor}` : ""].join("&");
+
+    let moreCursor = null;
+    let pullRequests = [];
+    try {
+      const respJson = await fetch(`${endpoint}?${params}`).then(r => r.json());
+      moreCursor = respJson.moreCursor;
+      pullRequests = respJson.pullRequests;
+    } catch (e) {
+      console.error("Error fetching whats-new", e);
+    }
+
+    let currentDate = this.state.currentDate;
+
+    for (let i = 0; i < pullRequests.length; i++) {
+      const pullRequest = pullRequests[i];
+      if (formatDate(pullRequest.mergedAt) === currentDate) {
+        pullRequest.mergedAt = null;
+      } else {
+        currentDate = formatDate(pullRequest.mergedAt);
+      }
+      pullRequest.body = md.render(pullRequest.body);
+    }
+
+    this.setState({
+      hasMore: !!moreCursor,
+      moreCursor,
+      currentDate,
+      pullRequests: [...this.state.pullRequests, ...pullRequests]
+    });
+  }
+  async render() {
+    const courses = await getCourses();
+    return (
+      <PageContainer>
+        <InfiniteScroll
+          loadMore={this.getWhatsNew.bind(this)}
+          hasMore={this.state.hasMore}
+          loader={
+            <div key="loader" className="loader">
+              <Spinner />
+            </div>
+          }
+          useWindow={false}
+          getScrollParent={() => document.body}
+        >
+          <div className="container">
+            <div className="main">
+              <div className="content">
+                <h1>
+                  <FormattedMessage id="whats-new-page.title" defaultMessage="My Course's" />
+                </h1>
+                <Row>
+                  {courses.map((course, index) => (
+                    <Col key={index} lg={4} md={6} sm={12}>
+                      {" "}
+                      {/* Use Bootstrap Col with size 4 for large screens */}
+                      <Course course={course} />
+                    </Col>
+                  ))}
+                </Row>
+              </div>
+            </div>
+          </div>
+        </InfiniteScroll>
+      </PageContainer>
+    );
+  }
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const container = document.getElementById("ui-root");
+
+  const root = createRoot(container);
+  root.render(
+    <WrappedIntlProvider>
+      <ThemeProvider store={store}>
+        <AuthContextProvider store={store}>
+          <Canvas />
+        </AuthContextProvider>
+      </ThemeProvider>
+    </WrappedIntlProvider>
+  );
+});
